548dd3c67d9d787bec9608605c02b1f05b5a6e8f
Wyatt Elliott <wyattelliott@Wyatts-MacBook-Pro.local>
2016-09-23 08:46:58 -0700

    activatedMongoLocally

--- server.js
+++ server.js
@@ -4,16 +4,58 @@ var app = express();
 var bodyParser = require('body-parser');
 var jsonParser = bodyParser.json();
 // var database = require('./database');
-var MongoClient = require('mongodb').MongoClient;
 var mongoose = require('mongoose');
+var db = mongoose.connection;
 
-mongoose.connect(process.env.MONGODB_URI);
+// db.on('error', console.error);
+// db.once('open', function() {
+//   Create your schemas and models here.
+// });
 
-mongoose.connection.on('error', function(err) {
-    console.error('Could not connect.  Error:', err);
+mongoose.connect('mongodb://localhost/test');
+
+
+
+var movieSchema = new mongoose.Schema({
+
+
 });
 
-console.log(process.env.MONGODB_URI);
+// Compile a 'Movie' model using the movieSchema as the structure.
+// Mongoose also creates a MongoDB collection called 'Movies' for these documents.
+var Movie = mongoose.model('Movie', movieSchema);
+
+
+
+
+var thor = new Movie({
+  title: 'Thor'
+, rating: 'PG-13'
+, releaseYear: '2011'  // Notice the use of a String rather than a Number - Mongoose will automatically convert this for us.
+, hasCreditCookie: true
+});
+
+thor.save(function(err, thor) {
+//  if (err) return console.error(err);
+//  console.dir(thor);
+console.log(thor);
+});
+
+
+
+
+
+
+
+// ------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------
+
+
+
+
+
+
+
 
 var orderSchema = mongoose.Schema({
     username: String,
@@ -54,6 +96,15 @@ var read = function(filter, callback) {
 };
 
 /* ----------------------------------------- */
+
+
+
+
+
+
+
+
+
 
 function requestHandler(request, response) {
     response.sendFile(__dirname + '/public/index.html');
